bf7b6ad1e8c8740c5ea7e2ae4bf9050f1b257d0b
yo-itz <yoitsu@gmail.com>
2024-02-17 15:37:21 +0900

    day2 added another object

--- Day2/Day2/ContentView.swift
+++ Day2/Day2/ContentView.swift
@@ -2,23 +2,40 @@ import SwiftUI
 import RealityKit
 
 struct ContentView: View {
-    
-    private let url = URL(string: "https://developer.apple.com/augmented-reality/quick-look/models/teapot/teapot.usdz")!
-    
+
+    private let url = URL(string:
+        "https://developer.apple.com/augmented-reality/quick-look/models/teapot/teapot.usdz"
+    )!
+    private let url2 = URL(string:
+        "https://developer.apple.com/augmented-reality/quick-look/models/cupandsaucer/cup_saucer_set.usdz"
+    )!
+
     var body: some View {
-        VStack {
-            Text("Show teapot")
+        HStack{
+            VStack {
+                Text("Show teapot")
+                Model3D(url: url) { model in
+                    model
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200, height: 200)
+                } placeholder: {
+                    ProgressView()
+                }
+            }.padding()
             
-            Model3D(url: url) { model in
-                model
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 200, height: 200)
-            } placeholder: {
-                ProgressView()
-            }
+            VStack {
+                Text("and teacup")
+                Model3D(url: url2) { model in
+                    model
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200, height: 200)
+                } placeholder: {
+                    ProgressView()
+                }
+            }.padding()
         }
-        .padding()
     }
 }
 
