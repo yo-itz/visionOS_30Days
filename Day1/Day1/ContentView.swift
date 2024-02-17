70dd6796782be65b1d772d6c07a8dc319c90d8db
yo-itz <yoitsu@gmail.com>
2024-02-17 14:39:34 +0900

    Tried Japanese

--- Day1/Day1/ContentView.swift
+++ Day1/Day1/ContentView.swift
@@ -4,6 +4,10 @@ struct ContentView: View {
 
     var body: some View {
         Text("Hello, visionOS!")
+        Text("こんにちは, visionOS!")
+        Text("私は漢字も表示できますか？\n改行はこうですね。(中央寄せ)")
+            .multilineTextAlignment(.center)
+            .padding(.top, 10.0)
     }
 }
 
